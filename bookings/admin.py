70ff80a7b70e6234e5f35c7f2dc6b7fb211b2d33
Nicole Rojas <nicole.eli.valdes@gmail.com>
2024-03-07 12:58:55 +0000

    Add resources model, views  and update html file

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Session, Therapist
+from .models import Booking 
 
 # Register your models here.
-admin.site.register(Session)
-admin.site.register(Therapist)
+admin.site.register(Booking)
+
 
 """
 @admin.register(UserProfile)
--- bookings/views.py
+++ bookings/views.py
@@ -1,23 +1,30 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Session, Therapist
+from django.shortcuts import render, redirect
+from .models import Specialist, Booking
 from .forms import BookingForm
 
+# Create your views here.
 
-class SessionListView(ListView):
-    template_name = "bookings/bookings.html"
-    model = Session
-    context_object_name = "bookings"
+def bookings(request):
+    form = BookingForm()
+    specialists = Specialist.objects.values_list('user__username', flat=True)
+    return render(request, 'bookings/bookings.html', {'form': form, 'specialists': specialists})
 
-def session(request):
+# @login_required
+def book_specialist(request, specialist_id):
+    specialist = Specialist.objects.get(pk=specialist_id)
+
     if request.method == 'POST':
         form = BookingForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('confirmation')
+            booking = form.save(commit=False)
+            booking.user = request.user
+            booking.specialist = specialist
+            booking.save()
+            return redirect('booking_success')  # Redirect to a success page
     else:
         form = BookingForm()
 
-    therapists = Therapist.objects.all()
-    context = {'form': form, 'therapists': therapists}
-    return render(request, 'bookings/bookings.html', context)
+    return render(request, 'book_specialist.html', {'form': form, 'specialist': specialist})
+
+def booking_success_view(request):
+    return render(request, 'bookings/booking_success.html')
